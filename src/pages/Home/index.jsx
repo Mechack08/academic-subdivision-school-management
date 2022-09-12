34053934d029ec4516e0b28d1eec49b0b914a32c
unknown <mechackkobius@gmail.com>
2022-09-12 09:03:10 +0300

    Some ajustement to fix the project

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './home.css'
 import Navbar from '../../components/Navbar'
 import Asidebar from '../../components/Asidebar'
+import HighLight from '../../components/highLight'
 
 const Home = () => {
   return (
@@ -9,6 +10,9 @@ const Home = () => {
       <Asidebar />
       <div className="principal">
         <Navbar />
+        <div className="main">
+          <HighLight />
+        </div>
       </div>
     </div>
   )
