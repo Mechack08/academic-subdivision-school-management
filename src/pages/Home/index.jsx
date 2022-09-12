2844e13d03a43a52b49a5ff1a6ec0b1a5457844e
unknown <mechackkobius@gmail.com>
2022-09-12 07:16:32 +0300

    design home.jsx file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './home.css'
 import Navbar from '../../components/Navbar'
+import Asidebar from '../../components/Asidebar'
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
+    <div className='home-container'>
+      <Asidebar />
+      <div className="principal">
+        <Navbar />
+      </div>
     </div>
   )
 }
