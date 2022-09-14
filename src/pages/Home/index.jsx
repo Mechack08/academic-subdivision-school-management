3ee9f1ccdf941a364fde7f9bdbe992060e950174
unknown <mechackkobius@gmail.com>
2022-09-14 08:07:20 +0300

    Ajustment

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,15 +3,19 @@ import './home.css'
 import Navbar from '../../components/Navbar'
 import Asidebar from '../../components/Asidebar'
 import HighLight from '../../components/highLight'
+import TopSchools from '../../components/topSchools'
 
 const Home = () => {
   return (
     <div className='home-container'>
-      <Asidebar />
+      <div className="aside">
+        <Asidebar />
+      </div>
       <div className="principal">
         <Navbar />
         <div className="main">
           <HighLight />
+          <TopSchools />
         </div>
       </div>
     </div>
