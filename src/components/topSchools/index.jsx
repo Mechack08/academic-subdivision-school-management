3141e5c09ee1f093a2fd45f1c921b98838228298
unknown <mechackkobius@gmail.com>
2022-09-16 15:34:01 +0300

    Getting the right way to handle the modal

--- src/components/topSchools/index.jsx
+++ src/components/topSchools/index.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import Ecoles from '../ecoles'
-import './topschool.css'
+import React from "react";
+import Ecoles from "../ecoles";
+import "./topschool.css";
+import { schools } from "../../data";
 
-const TopSchools = () => {
+const TopSchools = ({ showModal, getOneSchool }) => {
+  const secondaries = schools.filter((s) => s.category.name === "Secondaire");
+  secondaries.length = 5;
+
+  const primaries = schools.filter((p) => p.category.name === "Primaire");
+  primaries.length = 5;
+
   return (
-    <div className='topschool-container'>
-        <div className="school-section">
-          <h1 className="title">Top 5 Secondaires populaires</h1>
-          <Ecoles />
-        </div>
-        <div className="school-section">
-          <h1 className="title">Top 5 Primaires populaires</h1>
-          <Ecoles />
-        </div>
+    <div className="topschool-container">
+      <div className="school-section">
+        <h1 className="title">Top 5 Secondaires populaires</h1>
+        <Ecoles
+          data={secondaries}
+          showModal={showModal}
+          getOneSchool={getOneSchool}
+        />
+      </div>
+      <div className="school-section">
+        <h1 className="title">Top 5 Primaires populaires</h1>
+        <Ecoles
+          data={primaries}
+          showModal={showModal}
+          getOneSchool={getOneSchool}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopSchools
+export default TopSchools;
