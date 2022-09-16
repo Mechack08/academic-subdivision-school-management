d6ed5b9d41e59b15accd501fd91e0e1ca493d6ee
unknown <mechackkobius@gmail.com>
2022-09-16 15:36:25 +0300

    Fixing some issues, show Modal with data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import './home.css'
-import Navbar from '../../components/Navbar'
-import Asidebar from '../../components/Asidebar'
-import HighLight from '../../components/highLight'
-import TopSchools from '../../components/topSchools'
+import React, { useEffect, useState } from "react";
+import "./home.css";
+import Navbar from "../../components/Navbar";
+import Asidebar from "../../components/Asidebar";
+import HighLight from "../../components/highLight";
+import TopSchools from "../../components/topSchools";
+import Popup from "../../components/popup";
+import { schools } from "../../data";
 
 const Home = () => {
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [getSchool, setGetSchool] = useState([]);
+
+  // toggle popup
+  const showModal = () => {
+    setShowPopUp(true);
+  };
+
+  // get A school by it's ID
+  const getOneSchool = (id) => {
+    setGetSchool(schools.filter((s) => s.id === id));
+  };
+
   return (
-    <div className='home-container'>
+    <div className="home-container">
+      {showPopUp && (
+        <div className="popup-container show">
+          <Popup closeModal={setShowPopUp} school={getSchool} />
+        </div>
+      )}
       <div className="aside">
         <Asidebar />
       </div>
@@ -15,11 +35,11 @@ const Home = () => {
         <Navbar />
         <div className="main">
           <HighLight />
-          <TopSchools />
+          <TopSchools showModal={showModal} getOneSchool={getOneSchool} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
