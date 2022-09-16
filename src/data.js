35ae28343563c36cae9a5766f354b497b907f384
unknown <mechackkobius@gmail.com>
2022-09-16 15:37:44 +0300

    update data

---
+++ src/data.js
@@ -0,0 +1,431 @@
+export const schools = [
+      {
+        id: 1,
+        name: "Institut Malikiya Wa mbingu",
+        regime: "Convientionee Catolique",
+        pobox: "bp05678l",
+        created_date: "22-04-1986",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Secondaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 2,
+        name: "Institut Kasongomi",
+        regime: "Convientionee Catolique",
+        pobox: "0087654f",
+        created_date: "07-09-1998",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Secondaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 3,
+        name: "Les Rochers",
+        regime: "Privee",
+        pobox: "bp008976",
+        created_date: "07-09-2000",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Primaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 4,
+        name: "Mapambazuko",
+        regime: "Convientionee Adventiste",
+        pobox: "0087654f",
+        created_date: "07-09-1980",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Primaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 5,
+        name: "Institut Kambale PDIS",
+        regime: "Convientionee Catolique",
+        pobox: "0087654f",
+        created_date: "11-02-1974",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Secondaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 6,
+        name: "Institut Mutiri",
+        regime: "Convientionee Adventist",
+        pobox: "0087654f",
+        created_date: "22-11-1988",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Secondaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 7,
+        name: "EP Kilewi",
+        regime: "Convientionee Protestante",
+        pobox: "bp7654",
+        created_date: "07-09-1998",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Primaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 8,
+        name: "Institut Vungi",
+        regime: "Convientionee Catolique",
+        pobox: "0087654f",
+        created_date: "07-09-1998",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Secondaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 9,
+        name: "Institut Kitulu",
+        regime: "Convientionee Catolique",
+        pobox: "bp9870",
+        created_date: "15-05-1986",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Secondaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 10,
+        name: "EPA Kitulu One",
+        regime: "Convientionee Catolique",
+        pobox: "bp9320",
+        created_date: "15-05-1986",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Primaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      },
+      {
+        id: 11,
+        name: "EP Butenze",
+        regime: "Convientionee Catolique",
+        pobox: "bp9320",
+        created_date: "15-05-1986",
+        // Students
+        learners: {
+            total: 856,
+            filles: 467,
+            garcons: 389,
+            annee: 2022-2023
+        },
+        // School Type - Prim or Sec
+        category: {
+            id: 1,
+            name: 'Primaire'
+        },
+        // Leaders Table
+        dirigeant: {
+          id: 1,
+          nom: "Musubao",
+          postnom: 'Waliko',
+          prenom: 'Jean',
+          genre: "M",
+          birthday: "25-03-1989",
+          nationalite: 'Congolaise'
+        },
+        // Commune
+        commune: {
+            id: 1,
+            name: 'Mulekera'
+        },
+        // Commune
+        Quartier: {
+            id: 1,
+            name: 'Butenze'
+        }
+      }
+    ];
