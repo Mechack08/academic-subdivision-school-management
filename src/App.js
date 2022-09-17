0bd504c72155db07224de4330aa5ef9139611878
unknown <mechackkobius@gmail.com>
2022-09-17 14:16:36 +0300

    update structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Home from "./pages/Home";
-import './index.css'
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+import Asidebar from "./components/Asidebar";
 
 function App() {
   return (
-    <div>
-      <Home />
+    <div className="app-container">
+      <div className="asidebar">
+        <Asidebar />
+      </div>
+      <div className="right-side">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
