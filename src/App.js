5e9b357c29c6cc835621d824899e7eb83533148d
unknown <mechackkobius@gmail.com>
2022-09-17 18:34:35 +0300

    add useLocation Hook to Asidebar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Home from "./pages/Home";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Asidebar from "./components/Asidebar";
+import School from "./components/school";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation().pathname;
+
   return (
     <div className="app-container">
       <div className="asidebar">
-        <Asidebar />
+        <Asidebar location={location} />
       </div>
       <div className="right-side">
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/ecoles" element={<School />}></Route>
         </Routes>
       </div>
     </div>
