a236a0c9c9e5b7f11c1d45df6c9de8da0eeb3101
unknown <mechackkobius@gmail.com>
2022-09-30 11:58:14 +0300

    Updates to make it looks good

--- src/components/Asidebar/index.jsx
+++ src/components/Asidebar/index.jsx
@@ -10,26 +10,45 @@ import {
 import { Link } from "react-router-dom";
 
 const Asidebar = ({ location }) => {
+  console.log(location);
   return (
     <div className="asidebar-container">
       <span className="logo">LOGO SD BBO</span>
       <ul className="list-items">
-        <li className={location === "/" ? "active" : ""}>
-          <MdDashboard className="menu-icon" />
-          <Link to="/">Tableu de bord</Link>
-        </li>
-        <li className={location === "/ecoles" ? "active" : ""}>
-          <MdSchool className="menu-icon" />
-          <Link to="/ecoles">Ecoles</Link>
-        </li>
-        <li className={location === "/eleves" ? "active" : ""}>
-          <MdPeopleAlt className="menu-icon" />
-          <Link to="/eleves">Eleves</Link>
-        </li>
-        <li className={location === "/parametres" ? "active" : ""}>
-          <MdSettings className="menu-icon" />
-          <Link to="/parametres">Parametre</Link>
-        </li>
+        {location === "/" ||
+        location === "/ecoles" ||
+        location === "/eleves" ||
+        location === "/parametres" ? (
+          <>
+            <li className={location === "/" ? "active" : ""}>
+              <MdDashboard className="menu-icon" />
+              <Link to="/">Tableu de bord</Link>
+            </li>
+            <li className={location === "/ecoles" ? "active" : ""}>
+              <MdSchool className="menu-icon" />
+              <Link to="/ecoles">Ecoles</Link>
+            </li>
+            <li className={location === "/eleves" ? "active" : ""}>
+              <MdPeopleAlt className="menu-icon" />
+              <Link to="/eleves">Eleves</Link>
+            </li>
+            <li className={location === "/parametres" ? "active" : ""}>
+              <MdSettings className="menu-icon" />
+              <Link to="/parametres">Parametre</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className={location === "/ecole" ? "active" : ""}>
+              <MdDashboard className="menu-icon" />
+              <Link to="/ecole">Tableu de bord</Link>
+            </li>
+            <li className={location === "/ecole/eleves" ? "active" : ""}>
+              <MdPeopleAlt className="menu-icon" />
+              <Link to="/ecole/eleves">Eleves</Link>
+            </li>
+          </>
+        )}
       </ul>
       <div className="show-status">
         <div className="profile-pic">
