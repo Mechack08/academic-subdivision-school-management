25d1544cf7dc22813a94bb8be700fa4a86b3b9c8
unknown <mechackkobius@gmail.com>
2022-09-30 12:00:24 +0300

    Working on Navbar by adding props to make it reusable

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import Serchbar from '../serchBar'
-import {FaBell} from 'react-icons/fa'
-import './navbar.css'
+import React from "react";
+import Serchbar from "../serchBar";
+import { FaBell } from "react-icons/fa";
+import "./navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ title, dataHour }) => {
   return (
-    <div className='navbar-container'>
-        <div className="navbar-header">
-            <h1>Sous division Bbo</h1>
-            <span>Mercredi 07 Septembre 2022 | 14h55</span>
-        </div>
-        <div className="navbar-body">
-            <span className='msg'><FaBell /></span>
-            <Serchbar />
-        </div>
+    <div className="navbar-container">
+      <div className="navbar-header">
+        <h1>{title}</h1>
+        <span>{dataHour}</span>
+      </div>
+      <div className="navbar-body">
+        <span className="msg">
+          <FaBell />
+        </span>
+        <Serchbar />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
